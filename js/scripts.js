c3beca02de8093a96e03d85ea44b39c0d3924234
Josh Casuccio and Katy Henning <katy@>
2016-08-08 14:17:17 -0700

    adds bmi js script

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,12 @@ var division = function(number1, number2) {
  var number2 = parseInt(prompt("enter a number"));
  var result = add(number1, number2);
  alert(result);
+
+ // var weight = parseInt(prompt("What's your weight?"));
+ // var height = parseInt(prompt("What's your height?"));
+ //
+ // var calculate = function(weight, height){
+ // return (weight * 703) / (height * height);
+ // }
+ // var bmi = calculate(weight, height);
+ // alert("Your BMI is " + bmi);
