6064223499b8309014579172319aa14ac5539bdc
Josh Casuccio and Katy Henning <josh@>
2016-08-08 11:33:40 -0700

    adds basic calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var add = function(number1, number2) {
 	return number1 + number2;
  };
-
+ var subtract = function(number1, number2) {
+ 	return number1 - number2;
+ };
+ var multiplication = function(number1, number2) {
+ return number1 * number2;
+};
+var division = function(number1, number2) {
+	return number1 / number2;
+};
  var number1 = parseInt(prompt("enter a number"));
  var number2 = parseInt(prompt("enter a number"));
  var result = add(number1, number2);
