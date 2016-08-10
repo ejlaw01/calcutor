8af7d176e8d3c1295c0eae87bf8d0bd228f7f482
Ethan Law and Katy Henning <ethan@>
2016-08-10 14:49:53 -0700

    adds css styling, more javascript as well as a submit button and output area

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,31 @@
+//back-end logic:
+
 var add = function(number1, number2) {
 	return number1 + number2;
  };
- var subtract = function(number1, number2) {
+var subtract = function(number1, number2) {
  	return number1 - number2;
  };
- var multiplication = function(number1, number2) {
+var mutiply = function(number1, number2) {
  return number1 * number2;
 };
 var division = function(number1, number2) {
 	return number1 / number2;
 };
- var number1 = parseInt(prompt("enter a number"));
- var number2 = parseInt(prompt("enter a number"));
- var result = add(number1, number2);
- alert(result);
 
- // var weight = parseInt(prompt("What's your weight?"));
- // var height = parseInt(prompt("What's your height?"));
- //
- // var calculate = function(weight, height){
- // return (weight * 703) / (height * height);
- // }
- // var bmi = calculate(weight, height);
- // alert("Your BMI is " + bmi);
+//front-end logic
 
- // var fahrenheit = function(x) {
- // 	return x * 9 / 5 + 32;
- //   };
- //
- // var celcius = parseInt(prompt("Temperature in celcius:"));
- //
- // alert ("The answer is " + fahrenheit(celcius));
+$(document).ready(function() {
 
-//  var celcius = function(y) {
-// 	return y - 32 * 5 / 9;
-// };
-//
-// var fahrenheit = parseInt(prompt("Temperature in fahrenheit:"));
-//
-// alert ("The answer in celcius is " + celcius(fahrenheit));
+$("form#add").submit(function(event) {
+	event.preventDefault();
+	var add1 = parseInt($("#add1").val());
+	var add2 = parseInt($("#add2").val());
+	var result = add (add1, add2);
+	$("h2").show();
+	$("#output").text(result)
+});
+
+
+
+});
