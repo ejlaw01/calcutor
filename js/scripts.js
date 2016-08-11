86c1901fa9ac15f4af61ef4d2a42358439986c99
Ethan Law and Thomas Rivas <thomas@>
2016-08-11 15:51:26 -0700

    add radio interface to choose between 4 basic operations and write javascript to make functional

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,36 @@ var add = function(number1, number2) {
 var subtract = function(number1, number2) {
  	return number1 - number2;
  };
-var mutiply = function(number1, number2) {
+var multiply = function(number1, number2) {
  return number1 * number2;
 };
-var division = function(number1, number2) {
+var divide = function(number1, number2) {
 	return number1 / number2;
 };
-
 //front-end logic
 
 $(document).ready(function() {
 
-$("form#add").submit(function(event) {
+$("form#math").submit(function(event) {
 	event.preventDefault();
-	var add1 = parseInt($("#add1").val());
-	var add2 = parseInt($("#add2").val());
-	var result = add (add1, add2);
+	var num1 = parseFloat($("#num1").val());
+	var num2 = parseFloat($("#num2").val());
+	var operator = $("input:radio[name=operator]:checked").val();
+	var result;
+
+
+	if (operator === "add") {
+		result = add(num1, num2);
+	} else if (operator === "subtract") {
+		result = subtract(num1, num2);
+	} else if (operator === "multiply") {
+		result = multiply(num1, num2);
+	} else if (operator === "divide") {
+		result = divide(num1, num2);
+	}
+
 	$("h2").show();
 	$("#output").text(result)
 });
-
-
 
 });
